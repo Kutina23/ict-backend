a390514f8c84412d072eb2e0a018eb58f0747934
Mathew Kutina <kutinamathew6@gmail.com>
2025-12-28 02:31:34 -0800

    updated44

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Install Node.js and npm
+apt-get update
+apt-get install -y curl
+curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
+apt-get install -y nodejs
+
 # Install dependencies for both client and server
 cd client && npm install
 cd ../server && npm install
